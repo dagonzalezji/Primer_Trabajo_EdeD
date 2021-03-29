fd41257c18948007f09df29c5d21033a8253cf31
dagonzalezji <dagonzalezji@unal.edu.co>
2021-03-28 19:39:34 -0500

    ejemplo

--- src/Ajedrez.java
+++ src/Ajedrez.java
@@ -6,7 +6,7 @@ public class Ajedrez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int y = 0;
     }
     
 }
